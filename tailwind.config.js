3c5aa6893ed2c50a6abcd1f02a1036621c5575a2
azizNov <scorpionholiday72@gmail.com>
2023-11-25 11:13:24 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        "gray" : '#222831',
+      }
+    },
+  },
+  plugins: [],
+}
+
